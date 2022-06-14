813ce60c50f0c3acc410f87bafb1f6a7346314b9
Viraj <jadhavviraj50@gmail.com>
2022-06-14 19:27:28 +0530

    [Viraj]-Added UC3- Insert and retrive data from addressbook database

--- AddressBookWithADO/AddressBookWithADO/Program.cs
+++ AddressBookWithADO/AddressBookWithADO/Program.cs
@@ -1,12 +1,12 @@
 ﻿using AddressBook_ADO.NET;
 
-Console.WriteLine("Welcomw to AddressBook ADO.NET!");
+Console.WriteLine("Welcome to AddressBook ADO.NET!");
 
 
 AddressBookData addressBookData = new AddressBookData();
 
 
-Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase\n2)Create AddressBookTable");
+Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase\n2)Create AddressBookTable\n3)Inserting Details to DataBase\n4)retreiv Details From dataBase");
 int op = Convert.ToInt16(Console.ReadLine());
 switch (op)
 {
@@ -15,6 +15,22 @@ switch (op)
         break;
     case 2:
         addressBookData.CreateTables();
+        break;
+    case 3:
+        AddressBookModel addressbook = new AddressBookModel();
+        addressbook.FirstName = "Viraj";
+        addressbook.LastName = "Jadhav";
+        addressbook.Address = "Panvel";
+        addressbook.City = "Panvel";
+        addressbook.State = "Maharashtra";
+        addressbook.Zip = "410206";
+        addressbook.PhoneNumber = "1234567890";
+        addressbook.Email = "virajjadhav@gmail.com";
+        addressBookData.AddContact(addressbook);
+        Console.WriteLine("Record Inserted successfully");
+        break;
+    case 4:
+        addressBookData.GetAllContact();
         break;
     default:
         Console.WriteLine("Please choose the correct option!");
