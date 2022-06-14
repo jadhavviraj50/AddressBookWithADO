0afe52b8f2263ec151cfe0fb692b811e6df01a68
Viraj <jadhavviraj50@gmail.com>
2022-06-14 19:00:51 +0530

    [Viraj]-Added UC2- create table in address book database file

--- AddressBookWithADO/AddressBookWithADO/AddressBookData.cs
+++ AddressBookWithADO/AddressBookWithADO/AddressBookData.cs
@@ -26,6 +26,24 @@ namespace AddressBook_ADO.NET
             }
         }
 
+        /// Created Table in addressbook service database
+        public void CreateTables()
+        {
+            try
+            {
+                SqlConnection Connection = new SqlConnection(@"Data Source=DESKTOP-2DTGFII; Initial Catalog =AddressbookForADO; Integrated Security = True;");
+                Connection.Open();
+                SqlCommand command = new SqlCommand("Create table AddressBook(id int identity(1,1)primary key,FirstName varchar(200),LastName varchar(200),Address varchar(200), City varchar(200), State varchar(200), Zip varchar(200), PhoneNumber varchar(50), Email varchar(200)); ", Connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("AddressBook table has been  created successfully!");
+                Connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception occured while creating table:" + e.Message + "\t");
+            }
+        }
+
 
     }
 }
